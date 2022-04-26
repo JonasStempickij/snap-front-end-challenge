d95c68dd7f3b4f053b7f2d9f3a4fa51b7bbcd467
Jonas <jonasstempickij@gmail.com>
2022-04-26 16:01:21 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Content from './components/Content';
 
 function App() {
   return (
-    <div className='content'>
+    <>
       <Navbar />
       <Hero />
       <Content />
-    </div>
+    </>
   );
 }
 
--- src/assets/wrappers/Content.js
+++ src/assets/wrappers/Content.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.body``;
+const Wrapper = styled.body`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 2rem;
+`;
 
 export default Wrapper;
